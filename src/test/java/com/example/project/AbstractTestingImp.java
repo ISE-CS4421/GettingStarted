289c268c71d073638276ae8a31b0d3cbbe29b228
AdamD115114 <AdamDoherty1995@gmail.com>
2022-09-20 16:29:14 +0100

    Added test interface and broke down exercises into more managable sections

---
+++ src/test/java/com/example/project/AbstractTestingImp.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class AbstractTestingImp {
+    
+
+    public final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    public final PrintStream originalOut = System.out;
+
+
+    // Setup console streams
+    @BeforeEach
+    public void setupStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+    // Revert Setup
+    @AfterEach
+    public void restoreStreams() {
+        outContent.reset();
+        System.setOut(originalOut);
+    }
+}
--- src/test/java/com/example/project/SelectionMethodsTest.java
+++ src/test/java/com/example/project/SelectionMethodsTest.java
@@ -2,33 +2,12 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class SelectionMethodsTest {
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-
-
-    // Setup console streams
-    @BeforeEach
-    public void setupStreams(){
-        System.setOut(new PrintStream(outContent));
-    }
-    // Revert Setup
-    @AfterEach
-    public void restoreStreams() {
-        outContent.reset();
-        System.setOut(originalOut);
-    }
-
+public class SelectionMethodsTest extends AbstractTestingImp{
 
     @Test
     void testCanSleepIn() {
